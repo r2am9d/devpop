9839b6c2f6ea50aeefdb55de49f05d3cef6c3202
Ram Delatina <r2am9d@gmail.com>
2025-03-16 15:33:24 +0800

    Initial commit

---
+++ android/app/src/main/kotlin/com/neatadmiral/apps/devpop/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.neatadmiral.apps.devpop
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main_staging.dart
@@ -0,0 +1,6 @@
+import 'package:devpop/app/app.dart';
+import 'package:devpop/bootstrap.dart';
+
+void main() {
+  bootstrap(() => const App());
+}
